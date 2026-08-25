3c337e6f9a13f96506a0f8c36941d4fe018d6caa
prerana-ghub <prerana.ssesh@gmail.com>
2026-08-26 02:53:01 +0530

    S3 and IAM MFA checks
    The script checks S3 bucket public access settings and verifies if IAM users have MFA enabled.

---
+++ code/s3_and_iam_mfa_check.py
@@ -0,0 +1,60 @@
+#checks S3 public access & user mfa
+import json
+import boto3
+
+s3 = boto3.client("s3")
+iam = boto3.client("iam")
+
+def lambda_handler(event, context):
+
+    print("========== S3 SECURITY CHECK ==========")
+
+    buckets = s3.list_buckets()
+
+    for bucket in buckets["Buckets"]:
+
+        bucket_name = bucket["Name"]
+
+        print(f"\nChecking bucket: {bucket_name}")
+
+        try:
+            response = s3.get_public_access_block(
+                Bucket=bucket_name
+            )
+
+            config = response["PublicAccessBlockConfiguration"]
+
+            if (
+                config["BlockPublicAcls"] and
+                config["IgnorePublicAcls"] and
+                config["BlockPublicPolicy"] and
+                config["RestrictPublicBuckets"]
+            ):
+                print("✅ Bucket is protected from public access.")
+            else:
+                print("🚨 WARNING: Public access protection is disabled!")
+
+        except Exception as e:
+            print(f"Could not retrieve settings: {e}")
+
+    print("\n========== IAM MFA CHECK ==========")
+
+    users = iam.list_users()
+
+    for user in users["Users"]:
+
+        username = user["UserName"]
+
+        mfa = iam.list_mfa_devices(
+            UserName=username
+        )
+
+        if len(mfa["MFADevices"]) > 0:
+            print(f"✅ {username} - MFA Enabled")
+        else:
+            print(f"🚨 {username} - MFA NOT Enabled")
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("S3 and IAM security checks completed.")
+    }
